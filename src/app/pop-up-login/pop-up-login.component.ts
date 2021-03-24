5c4ae5405e9ed86a32e370b2a5a5565021c98ab3
mario <mrebollogarcia89@gmail.com>
2021-03-24 13:23:47 +0100

    [FIX] solucionado el error del popup

--- src/app/pop-up-login/pop-up-login.component.ts
+++ src/app/pop-up-login/pop-up-login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AfterViewInit, Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
@@ -9,26 +10,43 @@ import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 })
 export class PopUpLoginComponent implements OnInit, AfterViewInit {
   @ViewChild('visualizaModal', { static: false }) contenido: NgbModalRef;
-  @Output() enviarDatos = new EventEmitter<string>();
+  @Output() enviarDatosUsuario = new EventEmitter<string>();
+  @Output() enviarDatosLiga = new EventEmitter<[]>();
   nombre='';
   vermodal:boolean=false;
+  ligas=[];
   ngOnInit() {
-
+    // this.obtenerLigas();
+    console.log(this.ligas);
   }
   ngAfterViewInit() {
     // this.modalService.open(this.contenido);
   }
   closeResult: string;
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal,private http: HttpClient) { }
 
   open() {
     console.log(this.contenido);
     this.modalService.open(this.contenido);
   }
   enviar(){
-    this.enviarDatos.emit(
+    this.enviarDatosUsuario.emit(
       this.nombre
     );
+  }
+
+  obtenerLigas(){
+     
+    // this.http.get<any>("https://api-football-v1.p.rapidapi.com/v2/leagues/type/league", {
+    // headers: new HttpHeaders({
+    // "x-rapidapi-key": "8c028958c1msh04482df207ee55cp1d2d53jsn395d3438a725",
+    // "x-rapidapi-host": "api-football-v1.p.rapidapi.com"
+    // })
+    // }).subscribe(data => {
+    //    for (let index = 0; index < 100; index++) {
+    //      this.ligas.push(data.api.leagues[index]);
+    //    }
+    // });
   }
 }
