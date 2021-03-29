0d0f4465edda63d382dbaebd0057df4ae7d23561
mario <mariorebollogarcia89@gmail.com>
2021-03-29 12:36:37 +0200

    [ADD] listar jugadores

--- src/app/jugadores/jugadores.component.ts
+++ src/app/jugadores/jugadores.component.ts
@@ -8,12 +8,29 @@ import { PeticionesService } from '../servicios/peticiones.service';
   styleUrls: ['./jugadores.component.css']
 })
 export class JugadoresComponent implements OnInit {
-  equipoId:number
+  equipoId:number;
+  jugadores=[];
   constructor(private route:ActivatedRoute,private peticiones:PeticionesService) { }
 
   ngOnInit(): void {
-    
-  
+    this.route.params.subscribe(params=>{
+      console.log(params['equipo']);
+      this.equipoId=params['equipo'];
+      console.log(this.equipoId);
+    })
+    this.listarJugadores();
   }
-
+  //FILTRAR JUGADORES REPETIDOS
+  listarJugadores(){
+    this.peticiones.getJugadores(this.equipoId).subscribe(
+        data =>{
+          data.api.players.forEach(element => {
+            if(!this.jugadores.includes(element.player_name)){
+              this.jugadores.push(element);
+            }
+          });
+        }
+    )
+    console.log(this.jugadores);
+  }
 }
--- src/app/servicios/peticiones.service.ts
+++ src/app/servicios/peticiones.service.ts
@@ -30,4 +30,12 @@ export class PeticionesService {
             })
         })
      }
+     getJugadores(equipoId){
+        return this.http.get<any>('https://api-football-v1.p.rapidapi.com/v2/players/team/' + equipoId, {
+            headers: new HttpHeaders({
+                'x-rapidapi-key': '8c028958c1msh04482df207ee55cp1d2d53jsn395d3438a725',
+                'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
+            })
+        })
+     }
 }
